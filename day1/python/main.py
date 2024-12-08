73b66fe85581d9ccc85e9c948669d7e441d9634f
AbooMinister25 <aboominister@gmail.com>
2024-12-08 13:37:09 -0500

    Add day 1 solution

--- day1/python/main.py
+++ day1/python/main.py
@@ -1,14 +1,20 @@
-import more_itertools
-from aoc_lube import fetch, submit
+import functools
+from aoc_lube import fetch, submit  # type: ignore
 
 
-def part_1() -> None:
-    ...
+def part_1(inp: str) -> int:
+    numbers = [int(i) for i in inp.split()]
+    return sum([abs(a - b) for a, b in zip(sorted(numbers[::2]), sorted(numbers[1::2]))])
 
 
-def part_2() -> None:
-    ...
+def part_2(inp: str) -> int:
+    numbers = [int(i) for i in inp.split()]
+    second_row = numbers[1::2]
+
+    return sum(i * second_row.count(i) for i in numbers[::2])
 
 
 if __name__ == "__main__":
-    raw_input = fetch(2023, 1)
+    raw_input = fetch(2024, 1)
+    submit(2024, 1, 1, functools.partial(part_1, raw_input))
+    submit(2024, 1, 2, functools.partial(part_2, raw_input))
